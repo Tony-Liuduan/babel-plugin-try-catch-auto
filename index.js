7b8253135951b7fd3d4999e0c69823f995c9fee9
liuduan <liuduan.05.05@163.com>
2018-09-03 18:04:48 +0800

    兼容箭头函数处理

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const handleFuncBody = (path, _ref = { opts: {} }) => {
+    let funcBody = path.node.body.body
+    const funcId = path.node.id
+    const funcLoc = path.node.loc
+    let flag = false
+
+    if (!funcBody) {
+        flag = true
+        funcBody = path.node.body
+    }
+
+    if (funcBody && funcBody.length === 0) {
+        return
+    }
+
+    if (!funcLoc) {
+        return
+    }
+
+    const funcName = funcId ? funcId.name : 'anonymous function'
+    const funcLine = funcLoc.start.line
+    const funcErrorVariable = path.scope.generateUidIdentifier('e')
+
+    const astTemplate = flag ? wrapCaptureWithReturn : wrapCapture
+
+    const ast = astTemplate({
+        FUNC_BODY: funcBody,
+        FUNC_NAME: t.StringLiteral(funcName),
+        FUNC_LINE: t.NumericLiteral(funcLine),
+        ERROR_VARIABLE: funcErrorVariable
+    })
+
+    path.get('body').replaceWith(ast)
+}
+
 module.exports = function (babel) {
 
     const t = babel.types
@@ -6,9 +41,23 @@ module.exports = function (babel) {
         try {
             FUNC_BODY
         } catch (ERROR_VARIABLE) {
+            console.log('+++++++++++++++++++++')
             console.log(ERROR_VARIABLE)
             console.log(FUNC_NAME)
             console.log(FUNC_LINE)
+            console.log('+++++++++++++++++++++')
+        }
+    }`)
+
+    const wrapCaptureWithReturn = babel.template(`{
+        try {
+            return FUNC_BODY
+        } catch (ERROR_VARIABLE) {
+            console.log('+++++++++++++++++++++')
+            console.log(ERROR_VARIABLE)
+            console.log(FUNC_NAME)
+            console.log(FUNC_LINE)
+            console.log('+++++++++++++++++++++')
         }
     }`)
 
@@ -18,13 +67,85 @@ module.exports = function (babel) {
 
                 const funcBody = path.node.body.body
                 const funcId = path.node.id
-                const funcName = funcId ? funcId.name : 'anonymous function'
-                const funcLine = path.node.loc.start.line
-                const funcErrorVariable = path.scope.generateUidIdentifier('e')
+                const funcLoc = path.node.loc
 
                 if (funcBody.length === 0) {
                     return
                 }
+
+                if (!funcLoc) {
+                    return
+                }
+
+                const funcName = funcId ? funcId.name : 'anonymous function'
+                const funcLine = funcLoc.start.line
+                const funcErrorVariable = path.scope.generateUidIdentifier('e')
+
+
+                const ast = wrapCapture({
+                    FUNC_BODY: funcBody,
+                    FUNC_NAME: t.StringLiteral(funcName),
+                    FUNC_LINE: t.NumericLiteral(funcLine),
+                    ERROR_VARIABLE: funcErrorVariable
+                })
+
+                path.get('body').replaceWith(ast)
+            },
+            ArrowFunctionExpression(path) {
+                let funcBody = path.node.body.body
+                const funcId = path.node.id
+                const funcLoc = path.node.loc
+                let flag = false
+
+                if (!funcBody) {
+                    flag = true
+                    funcBody = path.node.body
+                }
+
+                if (funcBody && funcBody.length === 0) {
+                    return
+                }
+
+                if (!funcLoc) {
+                    return
+                }
+
+                const funcName = funcId ? funcId.name : 'anonymous function'
+                const funcLine = funcLoc.start.line
+                const funcErrorVariable = path.scope.generateUidIdentifier('e')
+
+                const astTemplate = flag ? wrapCaptureWithReturn : wrapCapture
+
+                const ast = astTemplate({
+                    FUNC_BODY: funcBody,
+                    FUNC_NAME: t.StringLiteral(funcName),
+                    FUNC_LINE: t.NumericLiteral(funcLine),
+                    ERROR_VARIABLE: funcErrorVariable
+                })
+
+                path.get('body').replaceWith(ast)
+            },
+            FunctionExpression(path) {
+                const funcBody = path.node.body.body
+
+                const funcId = path.node.id
+                const funcLoc = path.node.loc
+
+                if (!funcBody) {
+                    return
+                }
+
+                if (funcBody.length === 0) {
+                    return
+                }
+
+                if (!funcLoc) {
+                    return
+                }
+
+                const funcName = funcId ? funcId.name : 'anonymous function'
+                const funcLine = funcLoc.start.line
+                const funcErrorVariable = path.scope.generateUidIdentifier('e')
 
                 const ast = wrapCapture({
                     FUNC_BODY: funcBody,
